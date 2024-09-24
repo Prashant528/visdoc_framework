35868a0697dee7ed7f2666d383c584720ce42b13
Prashant528 <prashant.tandan528@gmail.com>
2024-09-23 23:38:46 -0700

    Added the dummy data

---
+++ visdoc_react/src/dummy_data.js
@@ -0,0 +1,66 @@
+const dummy_data = {
+    "CONTRIBUTING.md" : {
+        "Subheaders":{
+            "Code of Conduct":{
+                "links": {},
+                "summary":"Contains code of conduct"
+            },
+            "Contributor Guide":{
+                "links": {},
+                "summary":"Contains contributors guide"
+            },
+            "Setting up the dev environment":{
+                "links": {},
+                "summary":"How to setup the development environment?"
+            },
+            "Styling Code":{
+                "links": {
+                    "C++":"link to C++",
+                    "Objective C":"link to Objective C",
+                    "Java":"link to Java"
+                },
+                "summary":"Contains the guide for how to style the code."
+            },
+            "Testing":{
+                "links": {
+                    "Tree hygiene":"link to Tree hygiene",
+                    "Testing the engine wiki":"link to Testing the Engine wiki",
+                    "Skia Gold":"link to Skia Gold"
+                },
+                "summary":"Contains instruction for setting up the testing environment."
+            },
+        },
+
+        "Graph": ""
+    },
+
+    "link to Testing the Engine wiki" : {
+        "Subheaders":{
+            "C++ Core engine":{
+                "links": {},
+                "summary":"Contains code of conduct"
+            },
+            "Java Android Embedding":{
+                "links": {},
+                "summary":"Contains contributors guide"
+            },
+            "Objective C iOS embedding":{
+                "links": {},
+                "summary":"How to setup the development environment?"
+            },
+            "Dart dart-ui":{
+                "links": {},
+                "summary":"Contains the guide for how to style the code."
+            },
+            "Web Engine":{
+                "links": {},
+                "summary":"Contains instruction for setting up the testing environment."
+            },
+        },
+
+        "Graph": ''
+    },
+
+}
+
+export default dummy_data
