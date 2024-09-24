4b9e67f5392d56a8ed716881d97205b6ff0bfd69
Prashant528 <prashant.tandan528@gmail.com>
2024-09-24 13:23:11 -0700

    Changed dummy data into different format.

--- visdoc_react/src/dummy_data.js
+++ visdoc_react/src/dummy_data.js
@@ -1,66 +1,48 @@
-const dummy_data = {
-    "CONTRIBUTING.md" : {
-        "Subheaders":{
-            "Code of Conduct":{
-                "links": {},
-                "summary":"Contains code of conduct"
-            },
-            "Contributor Guide":{
-                "links": {},
-                "summary":"Contains contributors guide"
-            },
-            "Setting up the dev environment":{
-                "links": {},
-                "summary":"How to setup the development environment?"
-            },
-            "Styling Code":{
-                "links": {
-                    "C++":"link to C++",
-                    "Objective C":"link to Objective C",
-                    "Java":"link to Java"
-                },
-                "summary":"Contains the guide for how to style the code."
-            },
-            "Testing":{
-                "links": {
-                    "Tree hygiene":"link to Tree hygiene",
-                    "Testing the engine wiki":"link to Testing the Engine wiki",
-                    "Skia Gold":"link to Skia Gold"
-                },
-                "summary":"Contains instruction for setting up the testing environment."
-            },
-        },
-
-        "Graph": ""
+const dummy_data = [
+    {
+      "sequence": "Setting Up Environment for Development",
+      "edges": [
+        {"source": "Get the source", "target": "Get code dependencies"},
+        {"source": "Get code dependencies", "target": "Setup engine development environment"},
+        {"source": "Setup engine development environment", "target": "Setup Goma"}
+      ]
     },
-
-    "link to Testing the Engine wiki" : {
-        "Subheaders":{
-            "C++ Core engine":{
-                "links": {},
-                "summary":"Contains code of conduct"
-            },
-            "Java Android Embedding":{
-                "links": {},
-                "summary":"Contains contributors guide"
-            },
-            "Objective C iOS embedding":{
-                "links": {},
-                "summary":"How to setup the development environment?"
-            },
-            "Dart dart-ui":{
-                "links": {},
-                "summary":"Contains the guide for how to style the code."
-            },
-            "Web Engine":{
-                "links": {},
-                "summary":"Contains instruction for setting up the testing environment."
-            },
-        },
-
-        "Graph": ''
+    {
+      "sequence": "Contributing to Issue Triage",
+      "edges": [
+        {"source": "Triage an issue", "target": "Learn team triage process"},
+        {"source": "Learn team triage process", "target": "Determine actionable and non-actionable issues"},
+        {"source": "Determine actionable and non-actionable issues", "target": "Handle duplicates"},
+        {"source": "Handle duplicates", "target": "Assigning labels"},
+        {"source": "Assigning labels", "target": "Attend critical triage meeting"},
+        {"source": "Attend critical triage meeting", "target": "Issue hygiene"},
+        {"source": "Issue hygiene", "target": "About comments"},
+        {"source": "About comments", "target": "Locking an issue"},
+        {"source": "Locking an issue", "target": "Priorities"},
+        {"source": "Priorities", "target": "Assigning issues"},
+        {"source": "Assigning issues", "target": "Closing issues"}
+      ]
     },
-
-}
+    {
+      "sequence": "Helping in Documentation",
+      "edges": [
+        {"source": "Read the style guide", "target": "Help in API documentation"},
+        {"source": "Help in API documentation", "target": "Help in documentation issues"},
+        {"source": "Help in documentation issues", "target": "Get the flutter design document template"}
+      ]
+    },
+    {
+      "sequence": "Contributing to API Development",
+      "edges": [
+        {"source": "Help in development", "target": "Help in Quality Assurance"}
+      ]
+    },
+    {
+      "sequence": "Joining the Community",
+      "edges": [
+        {"source": "Read Code of Conduct", "target": "Join discord"}
+      ]
+    }
+  ]
 
 export default dummy_data
