18dfaba0693590ea34082a8efeaa318841835c1c
Prashant528 <prashant.tandan528@gmail.com>
2024-09-24 19:51:51 -0700

    Added code for text modal. Need to modify it.

--- visdoc_react/src/App.js
+++ visdoc_react/src/App.js
@@ -1,15 +1,22 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
 import {
   ReactFlow,
   useNodesState,
   useEdgesState,
   addEdge,
-  MarkerType
+  Controls,
+  NodeToolbar,
+  Handle,
+  Position
+  
 } from '@xyflow/react';
 import '@xyflow/react/dist/style.css';
 
 import render_graph from './graph_generator'
 import graph_sequences from "./dummy_data";
+import MyModal from './text_modal';
+
+const repo_name= 'Flutter'
 
 const initialNodes = [
   {
@@ -32,6 +39,7 @@ const initialNodes = [
     position: { x: 500, y: 100 },
     sourcePosition: 'right',
     targetPosition: 'left',
+    type:'node-with-toolbar'
   },
   {
     id: 'Node 4',
@@ -39,6 +47,7 @@ const initialNodes = [
     position: { x: 500, y: -100 },
     sourcePosition: 'right',
     targetPosition: 'left',
+    type:'node-with-toolbar',
   }
 ];
 
@@ -64,22 +73,80 @@ const initialEdges = [
   }
 ];
 
+function NodeWithToolbar({ data }) {
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
+  const [text, setText] = useState('');
+
+  const togglePopup = () => setIsPopupOpen(!isPopupOpen);
+  const handleTextChange = (e) => setText(e.target.value);
+
+  return (
+    <div style={{ padding: 10, border: '1px solid black', borderRadius: 5 }}>
+      <NodeToolbar
+        isVisible={data.forceToolbarVisible}
+        position={data.toolbarPosition}
+      >
+        <button onClick={togglePopup}>Text</button>
+        <button>Images</button>
+        <button>Videos</button>
+      </NodeToolbar>
+      <div className="new_node_label">{data.label}</div>
+      <Handle type="target" position={Position.Left} />
+      <Handle type="source" position={Position.Right} />
+
+      {isPopupOpen && (
+        <div
+          style={{
+            position: 'absolute',
+            top: 40,
+            left: 0,
+            padding: 10,
+            border: '1px solid gray',
+            backgroundColor: 'white',
+            zIndex: 100,
+            borderRadius: 5,
+          }}
+        >
+          <div style={{ marginBottom: 10 }}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </div>
+
+          <div>
+            <button onClick={togglePopup}>Close</button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
+
+const nodeTypes = {
+  'node-with-toolbar': NodeWithToolbar,
+};
+
+
 const App = () => {
-  let graph = render_graph(graph_sequences)
+  let graph = render_graph(repo_name, graph_sequences)
   // console.log(graph[0])
   // console.log(graph[1])
   let initialNodes = graph[0]
   let initialEdges = graph[1]
-  const [nodes, _, onNodesChange] = useNodesState(initialNodes);
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
   const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
   const onConnect = useCallback(
     (params) => setEdges((els) => addEdge(params, els)),
     [],
   );
+  
+  // const nodeTypes = useMemo(() => ({ toolbar_node: NodeWithToolbar }), []);
 
+  console.log(nodes)
+  console.log(edges)
 
+  
   return (
     <div style={{ height: 800 }}>
+    <MyModal text="Hello"/>
     <ReactFlow
       nodes={nodes}
       edges={edges}
@@ -88,7 +155,10 @@ const App = () => {
       onConnect={onConnect}
       fitView
       attributionPosition="bottom-left"
-    ></ReactFlow>
+      nodeTypes={nodeTypes}
+    >
+      <Controls />
+    </ReactFlow>
     </div>
   );
 };
--- visdoc_react/src/graph_generator.js
+++ visdoc_react/src/graph_generator.js
@@ -2,13 +2,14 @@ import {MarkerType} from '@xyflow/react';
 
 const x_offset = 300
 const y_offset = 300
-const edge_color = '#FF0072'
+const edge_color = '#007FFF'
 
-export default function render_graph(graph_sequences){
+export default function render_graph(repo_name, graph_sequences){
     // for (seq in graph_sequences){
     //     console.log(seq['sequence'], ':' )
     // }
     let nodes = []
+    nodes.push(create_parent_node(repo_name))
     let edges = []
     let center_of_y_axis = parseInt(graph_sequences.length/2) * y_offset
     let y_start_pos = -center_of_y_axis
@@ -24,6 +25,8 @@ export default function render_graph(graph_sequences){
                 nodes.push(new_node)
                 new_node = create_node(y_start_pos, i, 1, edge["target"])
                 nodes.push(new_node)
+                let new_edge = create_edge(repo_name, edge["source"])
+                edges.push(new_edge)
             }
             else{
                 let new_node = create_node(y_start_pos, i, j+1, edge["target"])
@@ -51,6 +54,8 @@ function create_node(y_start_pos, sequence_idx, edge_idx, node_name){
         position: { x: x_pos, y: y_pos },
         sourcePosition: 'right',
         targetPosition: 'left',
+        type:'node-with-toolbar'
+
       })
 }
 
@@ -70,5 +75,16 @@ function create_edge(source, target){
         strokeWidth: 2,
         stroke: edge_color,
         },
+      })
+}
+
+function create_parent_node(repo_name){
+    return({
+        id: repo_name,
+        data: { label: 'Contributing to '+repo_name },
+        position: { x: 0, y: 0 },
+        sourcePosition: 'right',
+        targetPosition: 'left',
+        type:'node-with-toolbar'
       })
 }
